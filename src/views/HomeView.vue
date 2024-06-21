63a768ce51402e0d4f1a2cd08cdcc25eb7be2535
hanhafizh <farhanhafizh770@gmail.com>
2024-06-21 20:22:58 +0700

    testing vue part 1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
+    <div class="container">
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Fluid jumbotron</h1>
+          <p class="lead">
+            This is a modified jumbotron that occupies the entire horizontal
+            space of its parent.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeView',
-  components: {
-  
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
